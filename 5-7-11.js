3ea6015cb495a061ace6ce38f5ece1127d975d5d
adrianvinuelas <adrian.vinuelas@gmail.com>
2015-03-23 17:49:19 +0100

    añadido polyfill geo.js y ejercicio terminado

--- 5-7-11.js
+++ 5-7-11.js
@@ -1,30 +1,37 @@
 jQuery(document).ready(function() {
 
-if (Modernizr.geolocation) {
-	console.log("existe geolocalizacion");
 	function showPosition(position) {
-		var coordenadas = "Latitude: " + position.coords.latitude +
-		"<br>Longitude: " + position.coords.longitude;
+			var coordenadas = "Latitude: " + position.coords.latitude +
+			"<br>Longitude: " + position.coords.longitude;
 		
-		var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13); //cargo el mapa
+			var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13); //cargo el mapa
 
-		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-		}).addTo(map);
+			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+			}).addTo(map);
 
-		L.marker([position.coords.latitude,  position.coords.longitude]).addTo(map)
-		    .bindPopup('LABORATORIO 3 <br> URJC')
-		    .openPopup();
+			L.marker([position.coords.latitude,  position.coords.longitude]).addTo(map)
+			    .bindPopup('LABORATORIO 3 <br> URJC')
+			    .openPopup();
 
-		$('#coord').html(coordenadas);
+			$('#coord').html(coordenadas);
 	}
 
-	navigator.geolocation.getCurrentPosition(showPosition);
+	if (Modernizr.geolocation) {
+		//navegador soporta geolocation, no hay problema
+		navigator.geolocation.getCurrentPosition(showPosition);
 	
-} else {
-	$('#coord').html("KEVIN ES UN PAQUETE, POR ESO NO TE RULA EL PROGRAMA");
-	//polyfill
-}
+	} else {
+		/*actua polyfill porque el navegador no lo soporta y en geo.js al final
+			if (!navigator.geolocation) {
+			    navigator.geolocation = new GeolocationPolyfill();
+			}
+		por eso cuando el navegador no tiene geolocation, navigator.geolocation se iguala a 
+		la funcion GeolocationPolifyll.
+		*/
+		navigator.geolocation.getCurrentPosition(showPosition);
+		
+	}
 
 });
 
